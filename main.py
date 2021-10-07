a7d8e727ae3cba27b1ecc89f2dca87d38bc286f1
faelaelae <jackman.12@msn.com>
2021-10-07 15:26:28 +1000

    updating main.py

--- main.py
+++ main.py
@@ -19,18 +19,29 @@ def createDB(csvPath):
     dbPath = "newdb.db"
     connectToDB(dbPath)
 
-def analysis1(dbPath):
-   pass
+def analysis1():
+    # Establish a connection to the database
+    c = connectToDB(dbPath)
+    
+    # Write up SQL command
+    sql_command = """
+        SELECT *
+        FROM fines
+        WHERE OFFENCE_DESC LIKE '%camera%' OR OFFENCE_DESC LIKE '%radar%';
+    """
+    
+    # Execute the SQL command
+    c.execute(sql_command)
+    
+    # Fetch the results of the query and store in variable
+    result = c.fetchall()
 def analysis2(code):
     c = connectToDB(dbPath)
-    print(c)
     sqlString = "SELECT OFFENCE_FINYEAR, count(*) as counter from fines WHERE OFFENCE_CODE = {code:.0f} GROUP by OFFENCE_FINYEAR"
     formattedSqlString = sqlString.format(code = code)
-    print(formattedSqlString)
     c.execute(formattedSqlString)
     x = c.fetchall()
     datalength = len(x)
-    print(len(x))
     # checking length on the result of the SQL query
     # returning an error msg if the length of x was 0
     if datalength == 0:
@@ -54,80 +65,63 @@ def analysis2(code):
     plt.show()
 
         
-def analysis3(dbPath, code):
-    pass
+def analysis3(startDate, endDate):
+    i = 0
 
+    # Establish a connection to the database
+    c = connectToDB(dbPath)
+    
+    # Write up SQL command
+    sql_command = """
+        SELECT *
+        FROM fines
+        WHERE OFFENCE_DESC LIKE '%camera%' OR OFFENCE_DESC LIKE '%radar%';
+    """
+    
+    # Execute the SQL command
+    c.execute(sql_command)
+    
+    # Fetch the results of the query and store in variable
+    result = c.fetchall()
+    
+    # Raw input (will be replaced with the input box)
+    rawStart = input("Input start period: ")
+    rawEnd = input("Input end period: ")
+    
+    # Split in start and end periods
+    splitStart = rawStart.split('-')
+    splitEnd = rawEnd.split('-')
+    
+    # Get period 1 (1st year of start period) & period 2 (2nd year of end period)
+    period1 = int(splitStart[0])
+    period2 = int(splitEnd[1])
+    
+    print(period1, period2)
+    
+    # For each record
+    for r in result:
+    
+        #
+        split_result_period = r[1].split('-')
+    
+        result_period1 = int(split_result_period[0])
+        result_period2 = int(split_result_period[1])
+    
+        if result_period1 >= period1 and result_period2 <= period2:
+            print(r)
+    
 
 def analysis4(option):
-    c = connectToDB(dbPath)
     if option == "trend":    
-        # SQL executions and converting to numpy arrays
-        c.execute("SELECT DISTINCT(OFFENCE_FINYEAR) FROM fines")
-        years2 = c.fetchall()
-        y2 = np.array(years2)
-        c.execute("SELECT count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' GROUP by OFFENCE_FINYEAR ORDER by OFFENCE_FINYEAR")
-        trenddata2 = c.fetchall()
-        td2 = np.array(trenddata2)
-        # converting years data into a readable list
-        y4 = list(set(y2.flat))
-        # sorting y4 so it matches the data from the database
-        y5 = y4.sort()
-        # plotting and showing graph
-        plt.plot(y4, td2)
-        plt.show()
+        a4Trend()
     if option == "codes":
-        c.execute("SELECT OFFENCE_CODE, count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' GROUP by OFFENCE_CODE ORDER by OFFENCE_CODE")
-        x = c.fetchall()        
-        data = dict(x)
-        keys = list(data.keys())
-        values = list(data.values())
-        
-        fixedkeys = list(map(str, keys))
-        
-        fig = plt.figure(figsize = (10, 5))
-        plt.bar(fixedkeys, values, color='maroon', width=0.4)
-        
-        plt.xlabel("Codes")
-        plt.ylabel("Occurences")
-        plt.title("Amount of times each mobile phone offence code occurs")
-        
-        plt.show()
+        a4Codes()
     if option == "schoolzonedata":
-        c.execute("SELECT OFFENCE_CODE, count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' AND SCHOOL_ZONE_IND ='Y' GROUP by OFFENCE_CODE ORDER by OFFENCE_CODE")
-        x = c.fetchall()
-        data = dict(x)
-        keys = list(data.keys())
-        values = list(data.values())
-        
-        fixedkeys = list(map(str, keys))
-        
-        fig = plt.figure(figsize = (10, 5))
-        
-        plt.bar(fixedkeys, values, color='maroon', width=0.4)
-        
-        plt.xlabel("Codes")
-        plt.ylabel("Occurences")
-        plt.title("Amount of times each code also occured in school zone")
-        
-        plt.show()
+        a4School()
     if option == "legislation":
-        c.execute("SELECT LEGISLATION, count(*) as legislationCount from fines WHERE MOBILE_PHONE_IND = 'Y' GROUP by LEGISLATION")
-        x = c.fetchall()
-        data = dict(x)
-        keys = list(data.keys())
-        values = list(data.values())
+        a4Legislation()
         
-        fixedkeys = list(map(str, keys))
-        
-        fig = plt.figure(figsize = (8, 8))
-        
-        plt.bar(fixedkeys, values, color='maroon', width=0.2)
-        
-        plt.xlabel("Legislation Act")
-        plt.ylabel("Occurences")
-        plt.title("Which legislation was used to fine")
-        
-        plt.show()    
+            
    
 def analysis5():
     c = connectToDB(dbPath)
@@ -148,102 +142,82 @@ def connectToDB(dbPath):
     c = conn.cursor()
     return c
 
-analysis2(6644)
-
-
-
-
-
-#leftover 
-#analysis 4 backup
-# c.execute("SELECT DISTINCT(OFFENCE_FINYEAR) FROM fines ")
-#     years = c.fetchall()
-#     c.execute("SELECT count(OFFENCE_FINYEAR) as FIN_YEAR FROM fines WHERE MOBILE_PHONE_IND == 'Y' GROUP BY OFFENCE_FINYEAR")
-#     yearlytrend = c.fetchall()
-#     c.execute("SELECT OFFENCE_FINYEAR, OFFENCE_CODE, count(*) FROM fines WHERE MOBILE_PHONE_IND == 'Y' GROUP BY OFFENCE_FINYEAR, OFFENCE_CODE")
-#     query = "SELECT OFFENCE_FINYEAR, OFFENCE_CODE, count(*) as COUNT FROM fines WHERE MOBILE_PHONE_IND == 'Y' GROUP BY OFFENCE_FINYEAR, OFFENCE_CODE"
-#df = pd.read_sql(trenddata, conn)
-    #plt.plot(years2, trenddata2)
-    #df = pd.read_sql(query, conn)
-    #plot=df.plot.bar(x='OFFENCE_FINYEAR', y='COUNT')
-    #codetrend = c.fetchall()
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-# #Analysing the cases caused by mobile phone usage - ie: trend over time, offence code, and so on.
-# import openpyxl
-# import datetime
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from dateutil import parser
-# from collections import Counter
-
-# path = "testdata.xlsx"
-
-# def open_Workbook(path):
-#     wb = openpyxl.load_workbook(path)
-#     return wb
-
-# wb = open_Workbook(path)
-
-# sheet = wb['data']
-
-# startDate = datetime.datetime(2012,1,1)
-# endDate = datetime.datetime(2013,12,12)
-
-
-# path = "testdata.xlsx"
-
-#     def open_Workbook(path):
-#         wb = openpyxl.load_workbook(path)
-#         return wb
-
-#     wb = open_Workbook(path)
-
-#     sheet = wb['data']
-
-#     startDate = datetime.datetime(2012,1,1)
-#     endDate = datetime.datetime(2013,12,12)
-
-#     def buildData(startDate, endDate, sheet):
-#         counter = 2
-#         data = []
-#         maxrows = sheet.max_row
-#         maxcolumn = sheet.max_column
-#         print(startDate, endDate)
-#         for row in sheet.iter_rows(min_row=1, max_col=maxcolumn,max_row=maxrows):
-#             counter += 1
-#             date = sheet.cell(row=counter,column=2).value
-#             if date is not None:
-#                 #date = parser.parse(d)
-#                 code = sheet.cell(row=counter,column=3).value
-#                 print(code)
-#                 if date >= startDate and date <= endDate:
-#                         data.append(code)
-#         print(data)
-#         return data
+def a4Trend():
+    c = connectToDB(dbPath)
+    # SQL executions and converting to numpy arrays
+    c.execute("SELECT DISTINCT(OFFENCE_FINYEAR) FROM fines")
+    years2 = c.fetchall()
+    y2 = np.array(years2)
+    c.execute("SELECT count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' GROUP by OFFENCE_FINYEAR ORDER by OFFENCE_FINYEAR")
+    trenddata2 = c.fetchall()
+    td2 = np.array(trenddata2)
+    # converting years data into a readable list
+    y4 = list(set(y2.flat))
+    # sorting y4 so it matches the data from the database
+    y5 = y4.sort()
+    # plotting and showing graph
+    plt.plot(y4, td2)
+    plt.show()
             
-#     listData = buildData(startDate, endDate, sheet)
-#     #def translateData(listData):
-#     #    countedList = Counter(listData)
-#     #    return countedList
+def a4Codes():
+    c = connectToDB(dbPath)
+    c.execute("SELECT OFFENCE_CODE, count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' GROUP by OFFENCE_CODE ORDER by OFFENCE_CODE")
+    x = c.fetchall()        
+    data = dict(x)
+    keys = list(data.keys())
+    values = list(data.values())
+    
+    fixedkeys = list(map(str, keys))
+    
+    fig = plt.figure(figsize = (10, 5))
+    plt.bar(fixedkeys, values, color='maroon', width=0.4)
+    
+    plt.xlabel("Codes")
+    plt.ylabel("Occurences")
+    plt.title("Amount of times each mobile phone offence code occurs")
+    
+    plt.show()
+    
+def a4School():
+    c = connectToDB(dbPath)
+    c.execute("SELECT OFFENCE_CODE, count(*) as Counter from fines WHERE MOBILE_PHONE_IND ='Y' AND SCHOOL_ZONE_IND ='Y' GROUP by OFFENCE_CODE ORDER by OFFENCE_CODE")
+    x = c.fetchall()
+    data = dict(x)
+    keys = list(data.keys())
+    values = list(data.values())
+    
+    fixedkeys = list(map(str, keys))
+    
+    fig = plt.figure(figsize = (10, 5))
+    
+    plt.bar(fixedkeys, values, color='maroon', width=0.4)
+    
+    plt.xlabel("Codes")
+    plt.ylabel("Occurences")
+    plt.title("Amount of times each code also occured in school zone")
+    
+    plt.show()
+            
+def a4Legislation():
+    c = connectToDB(dbPath)
+    c.execute("SELECT LEGISLATION, count(*) as legislationCount from fines WHERE MOBILE_PHONE_IND = 'Y' GROUP by LEGISLATION")
+    x = c.fetchall()
+    data = dict(x)
+    keys = list(data.keys())
+    values = list(data.values())
+    
+    fixedkeys = list(map(str, keys))
+    
+    fig = plt.figure(figsize = (8, 8))
+    
+    plt.bar(fixedkeys, values, color='maroon', width=0.2)
+    
+    plt.xlabel("Legislation Act")
+    plt.ylabel("Occurences")
+    plt.title("Which legislation was used to fine")
+    
+    plt.show()
 
-#     data = Counter(listData)
 
-#     def drawGraph(data):
-#         print(data.keys())
-#         print(data.values())
-#         index = np.arange(len(data.keys()))
-#         width = 0.2
-#         plt.bar(index, data.values(), width)
-#         plt.xticks(index + width * 0.5, data.keys())
-#         plt.show()
 
-#     drawGraph(data)
+
